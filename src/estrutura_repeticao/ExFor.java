01118d0f051f49d90245cbabb45763fd47325499
Ryansantos <ryan.alexandre054@outlook.com>
2024-12-05 15:40:02 -0300

    Atividade de for, while e do while

---
+++ src/estrutura_repeticao/ExFor.java
@@ -0,0 +1,29 @@
+package estrutura_repeticao;
+
+import java.util.Scanner;
+
+public class ExFor {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int totalPares = 0;
+		int totalImpares = 0;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print("Digite o " + i + "º número: ");
+			int numero = scanner.nextInt();
+
+			if (numero % 2 == 0) {
+				totalPares++;
+			} else {
+				totalImpares++;
+			}
+		}
+
+		System.out.println("Total de números pares: " + totalPares);
+		System.out.println("Total de números ímpares: " + totalImpares);
+
+	}
+
+}
